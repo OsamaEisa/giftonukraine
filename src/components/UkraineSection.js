7ccf838044594e1f43288f9a5b7a3c703459c4ab
Osama Mostafa Shehata <engosama46@yahoo.com>
2019-08-10 07:08:58 +0300

    Update UkraineSection.js

--- src/components/UkraineSection.js
+++ src/components/UkraineSection.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 const UkraineSection = () => {
   return (
@@ -8,25 +9,25 @@ const UkraineSection = () => {
           <h1 className="branch-header uk-h1">Ukraine</h1>
           <div className="row text-center">
             <div className="col-xs-12 col-sm-6 col-md-4">
-              <a href="studyInUkraine.html">
+              <Link to="/studyinukraine">
                 <i className="fas fa-university"></i>
                 <h3>Study in Ukraine</h3>
                 <p>The system of education provides good quality ...</p>
-              </a>
+              </Link>
             </div>
             <div className="col-xs-12 col-sm-6 col-md-4">
-              <a href="liveInUkraine.html">
+              <Link to="/liveinukraine">
                 <i className="fas fa-home"></i>
                 <h3>Living in Ukraine</h3>
                 <p>Accommodation in Ukraine is Cheaper according to ...</p>
-              </a>
+              </Link>
             </div>
             <div className="col-xs-12 col-md-4">
-              <a href="whyUkraine.html">
+              <Link to="/whyukraine">
                 <i className="fas fa-question-circle"></i>
                 <h3>Why to study in Ukraine</h3>
                 <p>No IELTS or TOEFL is required, Cost effective cheapest ...</p>
-              </a>
+              </Link>
             </div>
           </div>
         </div>
